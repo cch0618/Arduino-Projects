8fd06c4d0a26e09068497c1b55f0e0c24ee1f4dc
Donghyeok Tak <tdh8316@naver.com>
2018-11-17 18:41:54 +0900

    Update SmartHome.ino

--- SmartHome.ino
+++ SmartHome.ino
@@ -25,7 +25,10 @@ float temperature = 0.; //온도
 float discomfortIndex = 0.; //불쾌지수
 
 boolean isAirConditionerTurnOn = false;
+boolean isAirConditionerTurnOffByUser = false;
 boolean isBlindOpen = false;
+boolean isBlindCloseByUser = false;
+boolean isTVTurnOn = false;
 
 
 void setup() {
@@ -44,6 +47,65 @@ void setup() {
 }
 
 
+void turnOnTV(String content = "Hello, World!") {
+  TV.setCursor(0, 0);
+  TV.backlight();
+  TV.print(content);
+  isTVTurnOn = true;
+}
+
+void turnOffTV() {
+  TV.clear();
+  TV.noBacklight();
+  isTVTurnOn = false;
+}
+
+void turnOnAirConditioner() {
+  digitalWrite(pinAirConditioner, HIGH);
+  // 에어컨이 꺼져있는 상태였을 시 음악 재생
+  if (!isAirConditionerTurnOn) {
+    for (int i = 0; i < 8; i++) {
+      tone(pinPiezo, notes[i], 300); delay(100);
+    }
+    isAirConditionerTurnOn = true;
+  }
+}
+
+void turnOffAirConditioner() {
+  digitalWrite(pinAirConditioner, LOW);
+  isAirConditionerTurnOn = false;
+}
+
+void turnOnLights() {
+  //FIXME: 3색 LED 2개 각각의 RGB에 HIGH를 넘겨주면 색이 다름.
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  //digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  //digitalWrite(13, HIGH);
+}
+
+void turnOffLights() {
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+}
+
+void closeBlind() {
+  blind.attach(6); blind.write(180);
+  isBlindOpen = false;
+}
+
+void openBlind() {
+  blind.attach(6); blind.write(80);
+  isBlindOpen = true;
+}
+
+
 void loop() {
   // 온도센서로 온도와 습도 측정
   sensors_event_t event;
@@ -52,35 +114,38 @@ void loop() {
   temperature = event.temperature;
   // 불쾌지수 계산
   discomfortIndex = (1.8 * temperature) - (0.55 * (1 - humidity / 100.0) * (1.8 * temperature - 26)) + 32;
-  // 불쾌지수가 74 이상일 경우 에어컨 LED 작동
-  if (discomfortIndex >= 74) {
-    digitalWrite(pinAirConditioner, HIGH);
-    // 에어컨이 꺼져있는 상태였을 시 음악 재생
-    if (!isAirConditionerTurnOn) {
-      for (int i = 0; i < 8; i++) {
-        tone(pinPiezo, notes[i], 300); delay(100);
-      }
-      isAirConditionerTurnOn = true;
-    }
-  } else {
-    digitalWrite(pinAirConditioner, LOW);
-    isAirConditionerTurnOn = false;
+  Serial.println(discomfortIndex);
+  // 불쾌지수가 73 이상일 경우 에어컨 작동
+  if (discomfortIndex >= 73 && !isAirConditionerTurnOffByUser) turnOnAirConditioner();
+  else {
+    if (isAirConditionerTurnOffByUser) turnOffAirConditioner();
   }
 
   // 조도센서로 밝기 측정
   brightness = analogRead(pinCDS);
-  // 어두우면 블라인드를 내리고 전등을 킴
+  // 어두우면 블라인드를 내리고 모든 전등을 킴
   if (brightness < 600) {
-    if (isBlindOpen) {
-      blind.attach(6); blind.write(180);
-      isBlindOpen = false;
-    }
-    digitalWrite(11, HIGH);
+    if (isBlindOpen) closeBlind();
+    turnOnLights();
   } else {
-    if (!isBlindOpen) {
-      blind.attach(6); blind.write(80);
-      isBlindOpen = true;
+    if (!isBlindOpen) openBlind();
+    turnOffLights();
+  }
+
+  if (bluetooth.available()) {
+    char command = bluetooth.read();
+
+    if (command == '1') {
+      if (isTVTurnOn) turnOffTV();
+      else turnOnTV();
     }
-    digitalWrite(11, LOW);
+    else if (command == '2') {
+      turnOnAirConditioner();
+      isAirConditionerTurnOffByUser = false;
+    }
+    else if (command == '3') {
+      turnOffAirConditioner();
+      isAirConditionerTurnOffByUser = true;
+    }
   }
 }
